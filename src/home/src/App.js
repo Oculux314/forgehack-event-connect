c7d107bae2ab1bb0aa658d66a52abc87d77189fa
Oculux <nate.williamson.nz@gmail.com>
2024-07-02 18:03:59 +1200

    :sparkles: get data from backend

--- src/home/src/App.js
+++ src/home/src/App.js
@@ -1,9 +1,52 @@
+import { invoke } from "@forge/bridge";
+import { useEffect, useState } from "react";
+import { initialCauses, initialLocations } from "./initialState";
+
 function App() {
+  const [causes, setCauses] = useState(initialCauses);
+  const [locations, setLocations] = useState(initialLocations);
+
+  /* - Initial Data - */
+
+  async function getUserInfo() {
+    try {
+      const { options, cities } = await invoke("getUserInfo");
+      setCauses(options);
+      setLocations(cities);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  /* - Formatting - */
+
+  const noneSelected = <span className="grey">None Selected</span>;
+
+  function causesToString(causes) {
+    const causeString = causes
+      .filter((cause) => cause.chosen)
+      .map((cause) => cause.category)
+      .join(", ");
+    return causeString ?? noneSelected;
+  }
+
+  function locationsToString(locations) {
+    const locationString = locations
+      .filter((loc) => loc.chosen)
+      .map((loc) => loc.city)
+      .join(", ");
+    return locationString ?? noneSelected;
+  }
+
   return (
     <>
       <h1>Current Selections</h1>
-      <p>Causes: </p>
-      <p>Locations: </p>
+      <p>Causes: {causesToString(causes)}</p>
+      <p>Locations: {locationsToString(locations)}</p>
       <button>Edit Selections</button>
     </>
   );
---
+++ src/home/src/initialState.js
@@ -0,0 +1,20 @@
+export const initialCauses = [
+  { category: "Poverty", chosen: false },
+  { category: "Education", chosen: true },
+  { category: "Medical", chosen: false },
+  { category: "Environment", chosen: false },
+  { category: "Animals", chosen: false },
+  { category: "Human Rights", chosen: true },
+  { category: "Disaster Relief", chosen: false },
+  { category: "Global Warming", chosen: false },
+  { category: "Mental Health", chosen: false },
+];
+
+export const initialLocations = [
+  { city: "Sydney", chosen: false },
+  { city: "Melbourne", chosen: true },
+  { city: "Canberra", chosen: false },
+  { city: "Perth", chosen: true },
+  { city: "Adelaide", chosen: false },
+  { city: "Brisbane", chosen: false },
+];
