6ceeed4a55b3d9f56b3a28a3ff6930541b222525
Oculux <nate.williamson.nz@gmail.com>
2024-07-03 10:00:44 +1200

    :te

--- src/home/src/App.js
+++ src/home/src/App.js
@@ -51,6 +51,10 @@ function App() {
     setIsEdit((isEdit) => !isEdit);
   };
 
+  const goToDashboard = () => {
+    router.open("https://atl-forge-hack-team-2.atlassian.net/wiki/apps/eaf260e4-ff73-476a-a00f-cc421922aa68/9be46305-b0c9-4fe8-b67d-c4f97e26f7ec/preferences");
+  }
+
   return (
     <>
       {isEdit ? (
